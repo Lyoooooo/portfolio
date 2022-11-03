5022a2711579393f4c6120c95039c7f3c05a5776
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2022-11-03 10:28:31 +0100

    start

---
+++ main.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="portfolio.css">
+    <title>Portfolio</title>
+</head>
+<body>
+<div id="navhead" class="sticky-top">
+  <div id="nav">
+    <table id="tablenav">
+      <tr>
+        <td class="cellnav">option 1</td>
+        <td class="cellnav">option 2</td>
+        <td class="cellnav">option 3</td>
+        <td class="cellnav">option 4</td>
+        <td class="cellnav">option 5</td>
+      </tr>
+    </table>
+  </div>
+</div>
+<!-- <nav class="navbar navbar-expand-lg sticky-top" style="height: 80px; background-color:white;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="btn btn-outline-dark" role="button" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-outline-dark" role="button" href="#">Presentation</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-outline-dark" role="button" href="#">C.V.</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-outline-dark" role="button" href="#">Portfolio</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav> -->
+
+<div id="gblack">
+    <div class="carou">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="test1.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="test.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+        </div>
+        </div>
+        <div class="carousel-item">
+        <img src="rKVMCz8.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+        </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+    </div>
+</div>
+
+<div style="height: 600px; background-color: rgb(26, 26, 26); color: white;">
+  <div id="cartonpres" class="position-relative top-50 start-50 translate-middle">
+    <div id="cartonpres" class="position-relative top-50 start-50 translate-middle">
+    <div class="position-absolute top-40 start-100 translate-middle"><img src="pp.jpg" class="d-block w-20" style="width:150px;" alt="..."></div>
+      <h3>LEO TRIFFAULT</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus animi quasi pariatur vitae explicabo debitis rem veritatis vero id voluptatem! Ullam ab dolore sapiente quaerat quia soluta maxime magnam deserunt!Lorem
+
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+</body>
+</html>
